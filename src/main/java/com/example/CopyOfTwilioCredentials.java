7a217388386fa119b0327bc88b4f6da34d99a5c9
Matthew Galligan <mg@mg.org>
2014-08-12 13:29:06 -0400

    Added sample twill credentials file. Rename this class to TwillioCredentials

---
+++ src/main/java/com/example/CopyOfTwilioCredentials.java
@@ -0,0 +1,10 @@
+package com.example;
+/***
+ * Twilio credentials, don't post this to github!
+ *
+ */
+public class CopyOfTwilioCredentials {
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
+	
+}
